c131081b1f1b10ce7b4860f183bf129a2e4598c9
Daria Syusina <118437518+Syusina@users.noreply.github.com>
2023-10-01 12:17:06 +0500

    Update Shop.jsx

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import BrandBar from "../components/BrandBar";
 import DeviceList from "../components/DeviceList";
 import TypeBar from "../components/TypeBar";
+import styles from "./styles.module.css"
 
 const Shop = () => {
+  
+  const names = [
+    {
+    name: 'pickup',
+    index: 1
+  },
+
+    {name: 'courier',
+    index: 2
+  }];
+  let serverName = 'courier';
+  
+  const change = (e) => {
+    console.log(e.target)
+    serverName = e.target.value;
+  }
+
   return (
     <Container>
+    {names.map(({name, index}) => 
+      <div className={styles.container}>
+        <input
+          key={index}
+          type='radio'
+          name="delivery"
+          value={name}
+          checked={serverName === name}
+          onChange={change}
+        />
+        <label>{name}</label>
+      </div>
+    )}
+
       <Row className="mt-2">
         <Col md={3}>
           <TypeBar/>
